2d5af8ec03f07a8984f633a429ba6cd7a82ccbc0
Jay Jhangiani <47749517+jaydjhangiani@users.noreply.github.com>
2021-08-28 17:03:36 +0530

    added rel

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,6 +3,7 @@ import {
   FooterContainer,
   FooterWrap,
   SocialIconLink,
+  SocialIconDiv,
   SocialIcons,
   SocialLogo,
   SocialMedia,
@@ -33,16 +34,18 @@ const Footer = () => {
                 href="https://github.com/jaydjhangiani"
                 target="_blank"
                 aria-label="Github"
+                rel="noopener noreferrer"
               >
                 <FaGithub />
               </SocialIconLink>
-              <SocialIconLink aria-label="MADE WITH REACT">
+              <SocialIconDiv aria-label="MADE WITH REACT">
                 <FaReact />
-              </SocialIconLink>
+              </SocialIconDiv>
               <SocialIconLink
                 href="https://linkedin.com/in/jaydjhangiani"
                 target="_blank"
                 aria-label="Linkedin"
+                rel="noopener noreferrer"
               >
                 <FaLinkedin />
               </SocialIconLink>
